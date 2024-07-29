4b7ebc674af890f705aceba22fd51f5acfd4fe7a
SOMBIT MAZUMDER <115360765+SOMBIT4@users.noreply.github.com>
2024-07-29 15:07:49 +0600

    ui done

--- src/Count.jsx
+++ src/Count.jsx
@@ -1,3 +1,37 @@
 export default function Count() {
-    return <p>0</p>;
+  return <p className="count">0</p>;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*export default function Count({ size, number }) {
+  //const countName = props.number;
+  //const {size, number} = props;
+return (
+    <>
+    <CountTitle />
+    <p>{number}</p>
+    </>
+);
+}
+
+function CountTitle() {
+    return <h2>Current count is:</h2>;
+}
+*/
--- src/CountButton.jsx
+++ src/CountButton.jsx
@@ -1,10 +1,15 @@
+import { MinusIcon, PlusIcon } from "@radix-ui/react-icons";
 
 
 export default function CountButton() {
   return (
-    <div>
-    <button>-</button>
-    <button>+</button>
+    <div className="button-container">
+    <button className="count-btn">
+      <MinusIcon className="count-btn-icon"/>
+    </button>
+    <button className="count-btn">
+      <PlusIcon className="count-btn-icon"/>
+    </button>
   </div>
   );
 }
